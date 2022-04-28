be661b83e8ed9816305795cfb3ac05d917320444
unknown <ryancare@protonmail.com>
2022-04-28 15:49:38 -0500

    Tested all provided api endpoints and created fetch functions to harness each.

---
+++ frontend/fetch/index.js
@@ -0,0 +1,91 @@
+const login = async (creds) => {
+  const { username, password } = creds;
+  const token = await fetch("http://localhost:9000/api/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json.token)
+    .catch((err) => console.error(err));
+  localStorage.setItem("token", token);
+  // console.log(token, localStorage.getItem('token'));
+  return token;
+};
+
+const getArticles = async (token) => {
+  const articles = await fetch("http://localhost:9000/api/articles", {
+    method: "GET",
+    headers: {
+      Authorization: token,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json)
+    .catch((err) => console.error(err));
+  return articles;
+};
+
+const addArticle = async (token, article) => {
+  const { title, text, topic } = article;
+  const postRequest = await fetch("http://localhost:9000/api/articles", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      text,
+      topic,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => json);
+  return postRequest;
+};
+
+const editArticle = async (token, article, id) => {
+  const { title, text, topic } = article;
+  const putRequest = await fetch(`http://localhost:9000/api/articles/${id}`, {
+    method: "PUT",
+    headers: {
+      Authorization: token,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      text,
+      topic,
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => json);
+  return putRequest;
+};
+
+const deleteArticle = async (token, id) => {
+  const deleteRequest = await fetch(`http://localhost:9000/api/articles/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json);
+  return deleteRequest;
+};
+
+export { login, getArticles, addArticle, editArticle, deleteArticle };
